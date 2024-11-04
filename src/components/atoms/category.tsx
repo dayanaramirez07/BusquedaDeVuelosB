d2822089747351bd390f30f5b6c9302383931e91
dayanaramirez07 <dayana.ramirez@udea.edu.co>
2024-11-04 18:27:53 -0500

    Se elimina comentario

--- src/components/atoms/category.tsx
+++ src/components/atoms/category.tsx
@@ -80,47 +80,4 @@ const Category = ({ categories, setSelectedCategory, prices }: CategoryProps) =>
   );
 };
 
-export default Category; // Exporta el componente para que pueda ser usado en otros archivos
-
-
-/**
- * Category Component
- * 
- * Este componente renderiza una lista de categorías en formato de pestañas (tabs).
- * Permite al usuario seleccionar una categoría y muestra detalles como el precio
- * y los beneficios asociados a cada categoría.
- * 
- * Props:
- * - categories: Un array de objetos que contiene la información de cada categoría.
- *   Cada objeto debe tener las siguientes propiedades:
- *   - value: Identificador único de la categoría.
- *   - title: Título de la categoría.
- *   - description: Descripción breve de la categoría.
- *   - benefits: Un array de strings que describe los beneficios de la categoría.
- * 
- * - setSelectedCategory: Una función de callback que se ejecuta cuando el usuario selecciona
- *   una categoría. Recibe el objeto de la categoría seleccionada.
- * 
- * - prices: Un objeto que asocia cada categoría con su precio. La clave es el `value` de la
- *   categoría y el valor es el precio de la categoría.
- * 
- * Estado Local:
- * - selectedTab: Almacena el valor (`value`) de la categoría que está actualmente seleccionada.
- * 
- * Funcionalidad:
- * - Muestra las categorías en una lista de pestañas, permitiendo cambiar entre ellas.
- * - Al cambiar de pestaña, se actualiza el contenido mostrado, incluyendo el título,
- *   descripción, precio y beneficios de la categoría seleccionada.
- * - Cada categoría tiene un botón que, al hacer clic, selecciona la categoría y 
- *   ejecuta la función `setSelectedCategory` con los datos de la categoría seleccionada.
- * 
- * Componentes Utilizados:
- * - Button: Un botón reutilizable de la interfaz de usuario.
- * - Card: Un contenedor que agrupa los detalles de cada categoría.
- * - Tabs: Componente que organiza las categorías en pestañas.
- * - DialogClose: Un botón que cierra el diálogo al seleccionar una categoría.
- * 
- * Uso:
- * Este componente es ideal para casos en los que se necesita mostrar varias opciones
- * (como categorías de productos o planes) con sus detalles correspondientes.
- */
+export default Category;
